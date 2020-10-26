c6a28da29a55b42bcaaef4fef39f3a38ecc68ecd
Kévin Reilhac <kevin.reilhac@epitech.eu>
2020-10-26 14:13:24 +0100

    ADD: TextmeshPro ADD: Begin of card ScriptableObject Logic ADD: Card display from card data

---
+++ Assets/Scripts/DataModels/Cards/MonsterCardDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Monster card data model,
+/// Cards that placed on the board
+/// </summary>
+[CreateAssetMenu(fileName = "SpellCard", menuName = "Cards/Monster")]
+public class MonsterCardDataModel : BaseCardDataModel
+{
+    [Header("Monster data")]
+    public int LifePoints = 10;
+    public int Strenght = 10;
+}
---
+++ Assets/Scripts/DataModels/Cards/SpellCardDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Spell card data model,
+/// Cards that have effect on played
+/// </summary>
+[CreateAssetMenu(fileName = "SpellCard", menuName = "Cards/Spell")]
+public class SpellCardDataModel : BaseCardDataModel
+{
+    //[Header("Spell data")]
+    //WIP
+}
