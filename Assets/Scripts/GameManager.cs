36595b610e86256adbd2be44d6dfb81a40062ace
Kévin Reilhac <kevin.reilhac@epitech.eu>
2020-10-27 18:43:09 +0100

    Rework folders

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Manager<GameManager>
+{
+    private void OnEnable()
+    {
+        print(name + " created");
+    }
+}
