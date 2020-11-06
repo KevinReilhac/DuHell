3d08c1c5c6b04b6b06491369061408816bd9c7e1
Kévin Reilhac <kevin.reilhac@epitech.eu>
2020-11-06 17:24:52 +0100

    ADD: Unity tests setup

---
+++ Assets/Tests/Framework/CardList_tests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CardList_tests
+    {
+        private CardList cardList = new CardList();
+
+
+        [Test]
+        public void CardList_pop()
+        {
+            BaseCard card = new BaseCard();
+
+            card.Title = "TEST1";
+            cardList.Add(card);
+            BaseCard outCard = cardList.Pop();
+            Assert.AreEqual(cardList.Count, 0);
+            Assert.AreEqual(card, outCard);
+        }
+    }
+}
